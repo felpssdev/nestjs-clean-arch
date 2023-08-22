ec3ddbef31df6ca162a0522638b9cf77a92ac300
Felipe <felipeicloud11@gmail.com>
2023-08-22 18:57:14 -0300

    test: InMemoryRepository unit tests

--- src/shared/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -1,5 +1,7 @@
 import { Entity } from '@/shared/domain/entities/entity'
 import { InMemoryRepository } from '../../in-memory.repository'
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import { async } from 'rxjs'
 
 type StubEntityProps = {
   name: string
@@ -22,5 +24,50 @@ describe('InMemoryRepository unit tests', () => {
     await sut.insert(entity)
 
     expect(sut.items[0].toJSON()).toStrictEqual(entity.toJSON())
+  })
+
+  it('should throw NotFoundError when entity not found', async () => {
+    await expect(sut.findById('fake id')).rejects.toThrow(
+      new NotFoundError('Entity not found!'),
+    )
+  })
+
+  it('should return the correct entity when searching by id', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 10 })
+    await sut.insert(entity)
+
+    expect((await sut.findById(entity.id)).toJSON()).toStrictEqual(
+      entity.toJSON(),
+    )
+  })
+
+  it('should return all entities when finding all', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 10 })
+    await sut.insert(entity)
+
+    expect(await sut.findAll()).toStrictEqual([entity])
+  })
+
+  it('should throw NotFoundError when updating a non existent entity', () => {
+    const entity = new StubEntity({ name: 'test name', price: 10 })
+
+    expect(sut.update(entity)).rejects.toThrow(
+      new NotFoundError('Entity not found!'),
+    )
+  })
+
+  it('should update an existing entity', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 10 })
+
+    await sut.insert(entity)
+
+    const updatedEntity = new StubEntity(
+      { name: 'new name', price: 20 },
+      entity.id,
+    )
+
+    await sut.update(updatedEntity)
+
+    expect(sut.items[0].toJSON()).toStrictEqual(updatedEntity.toJSON())
   })
 })
