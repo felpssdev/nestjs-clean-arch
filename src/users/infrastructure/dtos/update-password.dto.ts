3c62f6c7c8ce5283c3ba48bf06e19553383a5fca
Felipe <felipeicloud11@gmail.com>
2023-10-03 15:29:53 -0300

    refactor: usecases dtos

---
+++ src/users/infrastructure/dtos/update-password.dto.ts
@@ -0,0 +1,9 @@
+import { UpdatePasswordUseCase } from '@/users/application/usecases/update-password.usecase'
+
+export class UpdatePasswordDto
+  implements Omit<UpdatePasswordUseCase.Input, 'id'>
+{
+  password: string
+  oldPassword: string
+  name: string
+}
--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -8,16 +8,16 @@ import {
   Delete,
 } from '@nestjs/common'
 import { UsersService } from './users.service'
-import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateUserDto } from './dto/update-user.dto'
+import { SignupDto } from './dtos/sign-up.dto'
+import { UpdateUserDto } from './dtos/update-user.dto'
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto)
+  create(@Body() signUpDto: SignupDto) {
+    return this.usersService.create(signUpDto)
   }
 
   @Get()
