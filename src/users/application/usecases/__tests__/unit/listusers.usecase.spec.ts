f8f0c2036d07871ac586b8f8159071697d5fdf91
Felipe <felipeicloud11@gmail.com>
2023-10-03 10:37:07 -0300

    test: listusers usecases unit tests

--- src/users/application/usecases/__tests__/unit/listusers.usecase.spec.ts
+++ src/users/application/usecases/__tests__/unit/listusers.usecase.spec.ts
@@ -13,7 +13,7 @@ describe('ListUsersUseCase unit tests', () => {
     sut = new ListUsersUseCase.UseCase(repository)
   })
 
-  it('should convert an instance of Search', () => {
+  it('should return converted SearchResult to Output', () => {
     let result = new UserRepository.SearchResult({
       items: [],
       total: 1,
@@ -52,6 +52,55 @@ describe('ListUsersUseCase unit tests', () => {
       currentPage: result.currentPage,
       lastPage: 1,
       perPage: result.perPage,
+    })
+  })
+
+  it('should return the users ordered by createdAt', async () => {
+    const createdAt = new Date()
+    const items = [
+      new UserEntity(UserDataBuilder({ createdAt })),
+      new UserEntity(
+        UserDataBuilder({ createdAt: new Date(createdAt.getTime() + 1) }),
+      ),
+    ]
+
+    repository.items = items
+
+    const output = await sut.execute({})
+    expect(output).toStrictEqual({
+      items: [...items].reverse().map(item => item.toJSON()),
+      total: 2,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 15,
+    })
+  })
+
+  it('should return the users using pagination, sort and filter', async () => {
+    const createdAt = new Date()
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'a' })),
+      new UserEntity(UserDataBuilder({ name: 'AA' })),
+      new UserEntity(UserDataBuilder({ name: 'Aa' })),
+      new UserEntity(UserDataBuilder({ name: 'b' })),
+      new UserEntity(UserDataBuilder({ name: 'c' })),
+    ]
+
+    repository.items = items
+
+    const output = await sut.execute({
+      page: 1,
+      perPage: 2,
+      sort: 'name',
+      sortDir: 'asc',
+      filter: 'a',
+    })
+    expect(output).toStrictEqual({
+      items: [items[1].toJSON(), items[2].toJSON()],
+      total: 3,
+      currentPage: 1,
+      lastPage: 2,
+      perPage: 2,
     })
   })
 })
