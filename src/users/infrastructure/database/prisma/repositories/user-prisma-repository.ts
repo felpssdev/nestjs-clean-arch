b35f58589ed1c05b3f9a795f7a52e0653ad119ff
Felipe <felipeicloud11@gmail.com>
2023-10-11 11:45:09 -0300

    feat: findByEmail and emailExists methods for userprismarepository

--- src/users/infrastructure/database/prisma/repositories/user-prisma-repository.ts
+++ src/users/infrastructure/database/prisma/repositories/user-prisma-repository.ts
@@ -3,18 +3,37 @@ import { PrismaService } from '@/shared/infrastructure/database/prisma/prisma.se
 import { UserEntity } from '@/users/domain/entities/user.entity'
 import { UserRepository } from '@/users/domain/repositories/user.repository'
 import { UserModelMapper } from '../models/user-model.mapper'
+import { ConflictError } from '@/shared/domain/errors/conflict-error'
 
 export class UserPrismaReposity implements UserRepository.Repository {
   sortableFields: string[] = ['name', 'createdAt']
 
   constructor(private prismaService: PrismaService) {}
 
-  findByEmail(email: string): Promise<UserEntity> {
-    throw new Error('Method not implemented.')
+  async findByEmail(email: string): Promise<UserEntity> {
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      return UserModelMapper.toEntity(user)
+    } catch (error) {
+      throw new NotFoundError(`UserModel not found using email ${email}`)
+    }
   }
 
-  emailExists(email: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async emailExists(email: string): Promise<void> {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (user) {
+      throw new ConflictError('Email address already in use')
+    }
   }
 
   async search(
