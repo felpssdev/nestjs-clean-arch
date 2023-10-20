d0871a24dc167275d4a63257142b8f785b3285eb
Felipe <felipeicloud11@gmail.com>
2023-10-20 14:36:58 -0300

    test: userpresenter unit tests

---
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -0,0 +1,37 @@
+import { instanceToPlain } from 'class-transformer'
+import { UserPresenter } from '../../user.presenter'
+
+describe('UserPresenter unit tests', () => {
+  let sut: UserPresenter
+  const createdAt = new Date()
+  const props = {
+    id: '8544973c-d1da-41e7-a4fd-f26d54d2f1b2',
+    name: 'fake name',
+    email: 'test@mail.com',
+    password: 'fakepassword',
+    createdAt,
+  }
+
+  beforeEach(() => {
+    sut = new UserPresenter(props)
+  })
+
+  describe('constructor', () => {
+    it('should be defined', () => {
+      expect(sut.id).toEqual(props.id)
+      expect(sut.name).toEqual(props.name)
+      expect(sut.email).toEqual(props.email)
+      expect(sut.createdAt).toEqual(props.createdAt)
+    })
+  })
+
+  it('should successfully present data', () => {
+    const output = instanceToPlain(sut)
+    expect(output).toStrictEqual({
+      id: '8544973c-d1da-41e7-a4fd-f26d54d2f1b2',
+      name: 'fake name',
+      email: 'test@mail.com',
+      createdAt: createdAt.toISOString(),
+    })
+  })
+})
